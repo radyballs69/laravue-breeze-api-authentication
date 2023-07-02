77b6c9170372becb9d9b52d6fa05a3f13a0a85eb
radyballs69 <geraldsedenio1@gmail.com>
2023-07-02 10:54:21 +0800

    Remove unnecessary codes

--- frontend/laravue-breeze/src/App.vue
+++ frontend/laravue-breeze/src/App.vue
@@ -1,23 +1,6 @@
 <script setup>
-import { ref, onMounted } from "vue";
 import { RouterView, useRouter } from "vue-router";
 import Nav from "@/views/layouts/Nav.vue";
-
-/* Using stores pinia */
-import { useAuthStore } from "@/stores/auth.js";
-
-// const router = useRouter();
-
-// router.beforeEach(async (to, from) => {
-//     const authStore = useAuthStore();
-//     await authStore.checkUserAuth();
-
-//     if (to.meta.middleware === "auth") {
-//         if (!authStore.isLoggedIn) return "/login";
-//     } else {
-//         if (authStore.isLoggedIn) return "/dashboard";
-//     }
-// });
 </script>
 <template>
     <header>
